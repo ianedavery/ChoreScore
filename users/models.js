2ac038f9321e7554d65939ac5b1923b27f74fbe1
Ian Avery <ianedavery@gmail.com>
2017-12-08 11:30:48 -0700

    added userschema and userschema method

--- users/models.js
+++ users/models.js
@@ -1,10 +1,14 @@
 'use strict';
 
+//importing the bcryptjs library. Used to encrypt passwords
 const bcrypt = require('bcryptjs');
+//importing mongoose. A library that enables communication between the app and the MongoDB
 const mongoose = require('mongoose');
 
+//allows mongoose to use ES6 Promises
 mongoose.Promise = global.Promise;
 
+//model for how the user data will be in mongo
 const UserSchema = mongoose.Schema({
 	username: {
 		type: String,
@@ -24,10 +28,26 @@ const UserSchema = mongoose.Schema({
 	}
 });
 
-UserSchema.methods.cleanRes = function() {
+//specifies what response objects should look like
+UserSchema.methods.serialize = function() {
 	return {
 		username: this.username,
 		firstName: this.firstName,
 		lastName: this.lastName
 	};
 };
+
+//compares user supplied, plain text, password with hashed password
+UserSchema.methods.validatePassword = function(password) {
+	return bcrypt.compare(password, this.password);
+};
+
+//hashes password with 12 rounds of salting
+UserSchema.statics.hashPassword = function(password) {
+	return bcrypt.hash(password, 12);
+};
+
+const User = mongoose.model('User', UserSchema);
+
+//exporting the mongoose user model
+module.exports = {User};
