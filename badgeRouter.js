801019c1152f116a4ba36dc0b85460ae1bb3ef92
Ian Avery <ianedavery@gmail.com>
2017-12-12 16:29:13 -0700

    added update to badge

--- badgeRouter.js
+++ badgeRouter.js
@@ -8,6 +8,11 @@ const bodyParser = require('body-parser');
 const {Badge} = require('./models');
 const router = express.Router();
 const jsonParser = bodyParser.json();
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+router.use(bodyParser.json());
 
 router.get('/', (req, res) => {
 	Badge
@@ -60,6 +65,25 @@ router.post('/', jsonParser, (req, res) => {
         console.error(err);
         res.status(500).json({message: 'Internal Server Error'});
     });
+});
+
+router.put('/:id', (req, res) => {
+	if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		res.status(400).json({
+			error: 'Request path id and request body id values must match'
+		});
+	}
+	const updated = {};
+	const updatableFields = ['badgename', 'badgeCost'];
+	updatableFields.forEach(field => {
+		if(field in req.body) {
+			updated[field] = req.body[field];
+		}
+	});
+	Badge
+		.findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+		.then(updatedBadge => res.status(204).end())
+		.catch(err => res.status(500).json({error: 'Something went wrong'}));
 });
 
 router.delete('/:id', (req, res) => {
