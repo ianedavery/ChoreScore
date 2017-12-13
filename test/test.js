ea6d5ef72cfa3b1dc421b606f2b07004e333f263
Ian Avery <ianedavery@gmail.com>
2017-12-12 17:17:51 -0700

    added arguments to runServer

--- test/test.js
+++ test/test.js
@@ -4,12 +4,13 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const {app, runServer, closeServer} = require('../server');
 const should = chai.should();
+const {TEST_DATABASE_URL} = require('../config');
 
 chai.use(chaiHttp);
 
 describe('Root', function() {
 	before(function() {
-		return runServer();
+		return runServer(TEST_DATABASE_URL);
 	});
 	after(function() {
 		return closeServer();
