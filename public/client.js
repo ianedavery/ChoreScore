5a24ef540d5874026b85c95db209124b74297b5b
Ian Avery <ianedavery@gmail.com>
2018-04-18 16:59:02 -0700

    added changed load anchor

--- public/client.js
+++ public/client.js
@@ -1322,7 +1322,7 @@ function handleChoreButtonClicks() {
 }
 
 function populateFamilyDashboard() {
-	$('#family-dashboard').load('/views/deleteFamily.html', event => {
+	$('#family-dashboard').load('https://morning-ocean-34038.herokuapp.com/views/deleteFamily.html', event => {
 		let cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 		$.get({
 			url: FAMILY_URL,
