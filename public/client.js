f510fb69f5753c747afd8ed7de112f89deb4c66b
Ian Avery <ianedavery@gmail.com>
2018-04-18 17:15:33 -0700

    added paren

--- public/client.js
+++ public/client.js
@@ -1322,8 +1322,7 @@ function handleChoreButtonClicks() {
 }
 
 function populateFamilyDashboard() {
-	$('#family-dashboard').load('../views/deleteFamily.html', event => {
-		event.preventDefault();
+	$('#family-dashboard').load(event => {
 		let cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 		$.get({
 			url: FAMILY_URL,
